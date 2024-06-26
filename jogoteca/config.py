dd050d91a2950772c3936ec17cf26379879310fe
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-06-26 14:33:11 -0300

    commit 2

---
+++ jogoteca/config.py
@@ -0,0 +1,14 @@
+from jogoteca import app
+
+
+SECRET_KEY = "alura"
+
+SQLALCHEMY_DATABASE_URI = (
+    "{SGBD}://{usuario}:{senha}@{servidor}/{database}".format(
+        SGBD="mysql+mysqlconnector",
+        usuario="root",
+        senha="",
+        servidor="localhost",
+        database="jogoteca",
+    )
+)
